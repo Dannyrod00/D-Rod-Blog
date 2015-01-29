0ab28d1b7748f8bb3c231e8d1e571353d6389aed
Daniel Rodriguez <dannyrodriguez822@gmail.com>
2015-01-29 09:20:13 -0800

    1/29/15

---
+++ controller/create-db.php
@@ -0,0 +1,8 @@
+<?php
+require_once ("../model/database.php ");
+
+$connection = new mysqli($host, $username, $password);
+
+if($connection=>connect_error) {
+    
+}
---
+++ model/database.php
@@ -0,0 +1,7 @@
+<?php
+$hoast = "Localhoast";
+$username = "root";
+$password = "root";
+$database = "blog_db";
+
+
