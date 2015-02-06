f449fc35f8e9d9e19806499b43014bcdfdd2ddd4
Daniel Rodriguez <dannyrodriguez822@gmail.com>
2015-02-06 09:20:51 -0800

    2/6/15

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
+
 <html>
     <head>
         <meta charset="UTF-8">
@@ -13,11 +14,16 @@ and open the template in the editor.
         
     </head>
     <body>
+        
+        <img src="http://media.giphy.com/media/RfJXiWNhSEshO/giphy.gif"> <br/>
+        
         <?php
 //          echo "Web Server is Running Locally";
 require_once (__DIR__ . "/controller/create-db.php");
-        ?>
+          
         
-        <a href="http://www.theappacademy.us/"><img src="http://media.giphy.com/media/RfJXiWNhSEshO/giphy.gif"></a>
+        ?> 
+        
+        
     </body>
 </html>
