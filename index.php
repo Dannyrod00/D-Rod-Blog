a3b406151b6902aa5596e36f0e1b4d547aec8717
Daniel Rodriguez <dannyrodriguez822@gmail.com>
2015-02-10 13:58:40 -0800

    2/10/15

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
-<!--
+
 <link type="text/css" rel="stylesheet" href="css/style.css">
-<img src="http://media.giphy.com/media/RfJXiWNhSEshO/giphy.gif"> <br/>-->
+<img src="http://media.giphy.com/media/RfJXiWNhSEshO/giphy.gif"> <br/>
                                           
     <?php
     require_once(__DIR__ . '/view/header.php');
