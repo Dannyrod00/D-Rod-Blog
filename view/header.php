79f7d0239cfab8c914a6d23ae12f2c02be328b73
Daniel Rodriguez <dannyrodriguez822@gmail.com>
2015-02-26 12:48:41 -0800

    music

--- view/header.php
+++ view/header.php
@@ -4,16 +4,14 @@
         <meta charset="UTF-8">
         <title>Blog</title>
         <link type="text/css" rel="stylesheet" href="css/style.css">
-        <link type="text/css" rel="stylesheet" href="css/bootstrap-theme.css">
-         <link type="text/css" rel="stylesheet" href="css/bootstrap.css">
-         
+       
         <link rel="icon" href="http://t1ak.roblox.com/04b920805e53152159a4853b695628a0" type="image/png">
     </head>
     <body>
         
         <!-- SCM Music Player http://scmplayer.net -->
 <script type="text/javascript" src="http://scmplayer.net/script.js" 
-data-config="{'skin':'skins/simpleOrange/skin.css','volume':100,'autoplay':true,'shuffle':true,'repeat':1,'placement':'top','showplaylist':true,'playlist':[{'title':'make the ground shake ','url':'https://www.youtube.com/watch?v=oeuVZ564-74'},{'title':'invaders theme song','url':'https://www.youtube.com/watch?v=8lAzm1vC7gw'},{'title':'demons','url':'https://www.youtube.com/watch?v=GFQYaoiIFh8'}]}" ></script>
+data-config="{'skin':'skins/purple/skin.css','volume':100,'autoplay':true,'shuffle':true,'repeat':1,'placement':'top','showplaylist':true,'playlist':[{'title':'make the ground shake ','url':'https://www.youtube.com/watch?v=oeuVZ564-74'},{'title':'invaders theme song','url':'https://www.youtube.com/watch?v=8lAzm1vC7gw'},{'title':'demons','url':'https://www.youtube.com/watch?v=GFQYaoiIFh8'}]}" ></script>
 <!-- SCM Music Player script end -->
 
 
@@ -21,10 +19,7 @@ data-config="{'skin':'skins/simpleOrange/skin.css','volume':100,'autoplay':true,
 
     <button type="button" draggable="true" class="btn btn-default navbar-btn">Sign in</button>
     
-    <audio controls autoplay draggable="true" title="Invaders theme" >
-        <source src="Space Invaders Extreme - Menu Song.mp3" type="audio/mpeg">
-
-</audio>
+  
         
     
 
